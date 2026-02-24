08ef7b2166991645e7a5c1994a7262552b3632ad
001Amit <118127587+001Amit@users.noreply.github.com>
2026-02-24 17:44:22 +0530

    Improve MongoDB connection logging
    Enhanced logging for MongoDB connection details.

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    console.log("MongoDB Connected Host:", conn.connection.host);
+    console.log("MongoDB Database Name:", conn.connection.name);
+    console.log("Mongo URI:", process.env.MONGO_URI);
   } catch (error) {
-    console.error(`MongoDB Error: ${error.message}`);
+    console.error("MongoDB error:", error.message);
     process.exit(1);
   }
 };
