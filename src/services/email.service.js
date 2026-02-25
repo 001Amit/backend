7f63834a3cdff7dc69f90f7b1b3085fac6f175b7
001Amit <118127587+001Amit@users.noreply.github.com>
2026-02-25 16:40:37 +0530

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev", // temporary test domain
+    from: "no-reply@deailyneedscarts.com",
     to,
     subject,
     html,
