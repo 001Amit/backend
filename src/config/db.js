591b4e3aa5168b5be8c3ef71c56a1788b7fad9bb
001Amit <118127587+001Amit@users.noreply.github.com>
2026-03-13 00:29:54 +0530

    Correct error handling in connectDB function
    Fix syntax error in connectDB function.

--- src/config/db.js
+++ src/config/db.js
@@ -3,14 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected Host:", conn.connection.host);
-    console.log("MongoDB Database Name:", conn.connection.name);
-  } catch (error) {
+    catch (error) {
     console.error("MongoDB error:", error.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
 
