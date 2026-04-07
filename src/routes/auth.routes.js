e0b2074a710830cca87800f29806fbfc06e1c85d
001Amit <118127587+001Amit@users.noreply.github.com>
2026-04-07 15:09:57 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,9 @@ import {
   verifyEmail,
   login,
   logout,
+  forgotPassword,
+  verifyResetOTP,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 import { authLimiter } from "../middleware/rateLimiter.js";
 
@@ -13,5 +16,7 @@ router.post("/register",authLimiter,register);
 router.post("/verify-email",authLimiter, verifyEmail);
 router.post("/login",authLimiter,login);
 router.post("/logout", logout);
-
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-reset-otp", verifyResetOTP);
+router.post("/reset-password", resetPassword);
 export default router;
